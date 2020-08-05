5d8147aebcfc5f0c93e3364310bdc4c12990299b
Priyanka Raval <priyanka.raval@easternts.com>
2020-08-05 19:03:39 +0530

    - set php start point to start vue application

---
+++ resources/views/spa.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="" type="image/png">
+    <title>Vue Demo</title>
+    <link href='https://fonts.googleapis.com/css?family=Rubik' rel='stylesheet'>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}" scoped>
+
+</head>
+<body>
+<div id="appMain" class="appMain"/>
+
+<script type="text/javascript" src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
+
+
+
