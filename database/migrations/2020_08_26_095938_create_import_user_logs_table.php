0a5c8ea03fdc0ec2cd577b9c05f5a8974c75176a
jenish prajapati <jenish.prajapati@easternts.com>
2020-08-26 16:44:34 +0530

    - API : user import : store error log in database

---
+++ database/migrations/2020_08_26_095938_create_import_user_logs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImportUserLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('import_user_logs', function (Blueprint $table) {
+            $table->increments('id')->index()->comment('AUTO_INCREMENT');
+            $table->string('filename',255)->nullable();
+            $table->longText('error_log')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('import_user_logs');
+    }
+}
