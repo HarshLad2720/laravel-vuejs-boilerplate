6626cd2aeda0ed76fbc80d72fbe39eed012570bc
AMIT MISTRY <jenish.prajapati@easternts.com>
2020-09-03 10:41:25 +0530

    verification code

--- app/Http/Controllers/API/User/VerificationAPIController.php
+++ app/Http/Controllers/API/User/VerificationAPIController.php
@@ -31,7 +31,7 @@ class VerificationAPIController extends Controller
         $user->status = config('constants.user.status_code.active');
         $user->save();
 
-        return redirect('home');
+        return redirect('');
 
     }
 
@@ -43,7 +43,7 @@ class VerificationAPIController extends Controller
     public function resend(Request $request)
     {
         if ($request->user()->hasVerifiedEmail()) {
-            return response()->json('User already have verified email!', config('constants.validation_codes.422'));
+            return response()->json('User already have verified email!', config('constants.validation_codes.unprocessable_entity'));
         }
         $request->user()->sendEmailVerificationNotification();
         return response()->json('The notification has been resubmitted');
